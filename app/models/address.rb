a2e6d86f988c629745fddaf37e90ebd7749508e6
Juan Lizarazo <gabriellg4@gmail.com>
2015-06-01 23:25:11 -0600

    Add address collection model

---
+++ app/models/address.rb
@@ -0,0 +1,11 @@
+class Address
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :street_address_1, type: String
+  field :street_address_2, type: String
+  field :city, type: String
+  field :state, type: String
+  field :zipcode, type: String
+  field :Country, type: String
+end
