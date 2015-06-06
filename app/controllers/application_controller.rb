3322e3ddd17b656410a87f192f8fb39f7ab92253
Juan Lizarazo <gabriellg4@gmail.com>
2015-06-06 16:40:13 -0600

    Support to switch between languages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_locale
+
+  private
+
+  # Set locale from URI param. When not available, default to english.
+  # If already set and not present on URI, use the one set in session.
+  def set_locale
+    if params.has_key?(:lang) && session[:locale] != params[:lang]
+      case params[:lang]
+      when 'en'
+        locale = :en
+      when 'es'
+        locale = :es
+      else
+        locale = :en
+      end
+
+      session[:locale] = locale
+    end
+
+    I18n.locale = session[:locale]
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,5 @@ module AddressBook
     # config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path +=
     # Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
   end
 end
