b102f2c9b2c481dddc7f6894911a1f156b7da8c7
Juan Lizarazo <gabriellg4@gmail.com>
2015-06-06 13:00:08 -0600

    Make init_embedded_attributes private. Include emails

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -14,11 +14,15 @@ class Contact
 
   accepts_nested_attributes_for :addresses, :phones, :emails
 
+  # Callbacks
   after_initialize :init_embedded_attributes
+
+  private
 
   # Init embedded documents when empty
   def init_embedded_attributes
     self.phones = [Phone.new] unless phones.count > 0
+    self.emails = [Email.new] unless phones.count > 0
     self.addresses = [Address.new] unless addresses.count > 0
   end
 end
